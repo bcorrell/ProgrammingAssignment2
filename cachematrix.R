cea9927e5b543784e934dd5afc4d6c9d123861c1
bcorrell <bcorrell@comcast.net>
2015-02-22 17:39:08 -0500

    First commit of programming assignment #2 to my repository.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Output value is a list.  Each element of the list is a function.
+  ## You can reference elements of a list by name like x$get.  So if 
+  ## you type x$get you will see the actual definition of the function.  
+  ## To call the function you can then use x$get().
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting inverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+
+
 }
